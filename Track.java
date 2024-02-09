2a8a971381a946a39370fd0f7946a3f96e1ce400
Noam Atzmon <noam.atzmon@post.idc.ac.il>
2024-02-09 17:29:40 +0200

    testing

--- Track.java
+++ Track.java
@@ -42,7 +42,6 @@ class Track {
     // Returns a string that represents the totalSeconds as "minutes:seconds",
     // Where seconds is always two digits. For example, "3:17" or "12:05".
     private String formattedDuration(int totalSeconds) {
-        String formattedSeconds = totalSeconds.format;
-        return formattedSeconds;
+        return "";
     }
 }
